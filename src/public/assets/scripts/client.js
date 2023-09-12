5ccd24ccc8477ffb8bb898bce6666c6ffad437f0
Michael Castro Montaño <86693441+michaelcm06@users.noreply.github.com>
2023-09-11 20:08:39 -0500

    Add files via upload

--- src/public/assets/scripts/client.js
+++ src/public/assets/scripts/client.js
@@ -1,27 +1,52 @@
-// Conectar al servidor a través de WebSocket
+
 const socket = io();
 
 // Escuchar eventos del servidor
-
-// Cuando se recibe la lista de productos del servidor
 socket.on('productos', (productos) => {
-  // Actualizar la lista de productos en la interfaz de usuario
+  // Llamar a una función para actualizar la lista de productos en la interfaz de usuario
   updateProductList(productos);
 });
 
+// Función para actualizar la lista de productos en la interfaz de usuario
+function updateProductList(productos) {
+  const productList = document.getElementById('productList');
+  productList.innerHTML = '';
+
+  productos.forEach((producto) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${producto.nombre} -
+       ${producto.descripcion} - 
+       $${producto.precio}`;
+
+      const eliminarButton = document.createElement('button');
+      eliminarButton.className = 'eliminarProducto';
+      eliminarButton.dataset.id = producto.id;
+      eliminarButton.textContent = 'Eliminar';
+
+      listItem.appendChild(eliminarButton);
+      productList.appendChild(listItem);
+  });
+}
+
+
 // Manejar el envío de un nuevo producto desde el formulario
 const agregarProductoForm = document.getElementById('agregarProductoForm');
-agregarProductoForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+agregarProductoForm.addEventListener('submit', () => {
+  // Evitar que se realice la acción por defecto del formulario (recargar la página)
+  event.preventDefault();
 
   const nombre = document.getElementById('nombre').value;
-  const descripcion = document.getElementById('descripcion').value; // Nueva línea para descripción
+  const descripcion = document.getElementById('descripcion').value;
   const precio = parseFloat(document.getElementById('precio').value);
 
   // Enviar el nuevo producto al servidor a través de WebSocket
   socket.emit('nuevoProducto', { nombre, descripcion, precio });
-});
 
+  // Limpiar los campos del formulario después de enviar
+  document.getElementById('nombre').value = '';
+  document.getElementById('descripcion').value = '';
+  document.getElementById('precio').value = '';
+});
 // Manejar el clic en el botón "Eliminar" para eliminar un producto
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('eliminarProducto')) {
@@ -32,22 +57,4 @@ document.addEventListener('click', (e) => {
   }
 });
 
-// Función para actualizar la lista de productos en la interfaz de usuario
-function updateProductList(productos) {
-  // Actualiza la lista de productos en la interfaz de usuario (por ejemplo, utilizando DOM manipulation)
-  const productList = document.getElementById('productList');
-  productList.innerHTML = ''; // Limpia la lista antes de actualizar
 
-  productos.forEach((producto) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<strong>${producto.nombre}</strong> - ${producto.descripcion} - $${producto.precio}`;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Eliminar';
-    deleteButton.className = 'eliminarProducto';
-    deleteButton.dataset.id = producto.id;
-
-    listItem.appendChild(deleteButton);
-    productList.appendChild(listItem);
-  });
-}
